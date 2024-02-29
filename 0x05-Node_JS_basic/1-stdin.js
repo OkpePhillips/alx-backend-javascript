28b848e49abf6762ba071d26497c90ead63eaff8
Godwin Okpe <okpegodwin18@yahoo.com>
2024-02-29 10:52:15 +0000

    USing the readline module to achieve the expected result

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,21 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+const readline = require('readline');
 
-process.stdin.once('data', function(data) {
-  const name = data.toString().trim();
+// Creating an interface to read input from the console
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-  console.log("Your name is:", name);
-  console.log("This important software is now closing");
-  process.exit();
+// Displaying a message to the user
+console.log('Welcome to Holberton School, what is your name?');
+
+// Listening for user input
+rl.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+  rl.close();
+});
+
+// Handling the close event
+rl.on('close', () => {
+  console.log('This important software is now closing');
 });
